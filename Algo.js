c3630180c55decebb9cf6752e691d76c95622215
gandholi-kumar <46054848+gandholi-kumar@users.noreply.github.com>
2022-11-05 13:31:39 +0530

    Find most repeated value

--- Algo.js
+++ Algo.js
@@ -1,9 +1,47 @@
-// Sum of N numbers
-console.log(sumOfN(1)(2)(4)());
+/*
+  Sum of N numbers
+*/
 function sumOfN(a) {
   return function (b) {
     return b ? sumOfN(a + b) : a;
   };
 }
 const ES6MultipleOfN = (a) => (b) => b ? ES6MultipleOfN(a * b) : a;
-console.log(ES6MultipleOfN(1)(2)(4)());
+// console.log(ES6MultipleOfN(1)(2)(4)());
+// console.log(sumOfN(1)(2)(4)());
+
+/*
+  Find the most repeated value
+*/
+const arr = ['one', 'three', 'two', 'two', 'two', 'three', 'two', 'three'];
+const arr1 = ['one'];
+
+function maxIteratedValue(arr) {
+  let map = new Map();
+  let maxItergatedVal = 0;
+
+  arr.forEach((val) => {
+    if (map.has(val)) {
+      map.set(val, map.get(val) + 1);
+      maxItergatedVal < map.get(val)
+        ? (maxItergatedVal = map.get(val))
+        : maxItergatedVal;
+    } else {
+      map.set(val, 1);
+      maxItergatedVal < map.get(val)
+        ? (maxItergatedVal = map.get(val))
+        : maxItergatedVal;
+    }
+  });
+
+  return { map, maxItergatedVal };
+}
+
+const { map, maxItergatedVal } = maxIteratedValue(arr);
+
+let maxList = [];
+for (let val of map.entries()) {
+  if (maxItergatedVal === val[1]) maxList.push(val);
+}
+
+console.log(maxList);
--- practice.js
+++ practice.js
@@ -14,4 +14,67 @@ function fun(a, b, c) {
   console.log(this === obj, a, b, c);
 }
 
-fun.myCall(obj, 1, 2, 3);
+// fun.myCall(obj, 1, 2, 3);
+
+// Generator function
+let counter = 0;
+
+function* printNumbers() {
+  let timer = setTimeout(() => {
+    counter++;
+  }, 100);
+  console.log(timer);
+  console.log(counter);
+
+  yield counter;
+  counter += timer;
+  yield counter;
+
+  counter -= timer;
+  yield counter;
+}
+
+const k = printNumbers();
+
+// console.log(k.next());
+// setTimeout(() => {
+//   console.log(k.next());
+// }, 1000);
+// setTimeout(() => {
+//   console.log(k.next());
+// }, 5000);
+// setTimeout(() => {
+//   console.log(k.next());
+// }, 5000);
+// console.log(k.next());
+// for (let value of k ){
+//   console.log(value);
+// }
+
+
+function *custGen() {
+  const vlaues = 'values1';
+  console.log(vlaues);
+  let x = yield '1';
+
+  console.log(x);
+  console.log('some one ' + vlaues);
+  yield '2';
+  let y = yield;
+  console.log('testing 2');
+  console.log(y);
+  yield '3';
+}
+
+const genCust = custGen();
+
+// console.log(genCust.next());
+// console.log(genCust.next(2));
+// console.log(genCust.next());
+// console.log(genCust.next());
+// console.log(genCust.next(3));
+
+// ----------------------------------------------- //
+
+// ----------------------------------------------- //
+
